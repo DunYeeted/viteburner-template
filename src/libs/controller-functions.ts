a6494296e4c0a6ea55c15d7c1a6fe69a43dfd054
DunYeeted <75920735+DunYeeted@users.noreply.github.com>
2026-01-03 16:29:11 -0600

    Fixed RamNet Added more interfaces

--- src/libs/controller-functions.ts
+++ src/libs/controller-functions.ts
@@ -1,24 +1,25 @@
 import { NS } from '@ns';
 import { ExpandedNS } from './ExpandedNS';
 
-export class RamNet extends Array {
+export class RamNet {
+  private network: { server: string, ram: number }[];
   constructor(nsx: ExpandedNS) {
-    super();
     const servers = nsx.fullScan();
+    this.network = [];
     for (let i = 0; i < servers.length; i++) {
-      this[i] = [servers[i], nsx.emptyRam(servers[i])];
+      this.network[i] = { server: servers[i], ram: nsx.emptyRam(servers[i]) };
     }
     this.sortNetwork();
   }
 
   get largestServer(): string {
     this.sortNetwork();
-    return this[0][0];
+    return this.network[0].server;
   }
 
   private sortNetwork(): void {
-    this.sort((a, b) => {
-      return a[1] - b[1];
+    this.network.sort((a, b) => {
+      return a.ram - b.ram;
     });
   }
 
@@ -28,12 +29,12 @@ export class RamNet extends Array {
    * @example findSuitableServer(1.70); // Returns 'n00dles'
    */
   public findSuitableServer(ram: number): string | undefined {
-    const s = this.find((server) => {
-      server[1] > ram;
+    const s = this.network.find((server) => {
+      server.ram > ram;
     });
 
     if (s == undefined) return undefined;
-    return s[0];
+    return s.server;
   }
 
   /**
@@ -43,13 +44,13 @@ export class RamNet extends Array {
   public reserveRamOnServer(server: string | undefined, ram: number): void {
     if (server == undefined) return;
 
-    const s = this.find((s) => {
-      return s[0] === server;
+    const s = this.network.find((s) => {
+      return s.server === server;
     });
 
     if (s == undefined) throw new Error(`${server} not defined on network!`);
 
-    s[1] -= ram;
+    s.ram -= ram;
 
     this.sortNetwork();
     return;
@@ -61,13 +62,13 @@ export class RamNet extends Array {
   public undoReserve(server: string | undefined, ram: number): void {
     if (server == undefined) return;
 
-    const s = this.find((s) => {
-      return s[0] === server;
+    const s = this.network.find((s) => {
+      return s.server === server;
     });
 
     if (s == undefined) throw new Error(`${server} not defined on network!`);
 
-    s[1] += ram;
+    s.ram += ram;
 
     this.sortNetwork();
     return;
@@ -131,17 +132,30 @@ export function isServerDefined(j: Job) {
   return j.server == undefined;
 }
 
-export interface Batch {
-  readonly hack: Job;
-  readonly weaken1: Job;
-  readonly grow: Job;
-  readonly weaken2: Job;
+// Only for experience farm batchers
+export interface IGBatch {
+  grow: Job;
+}
+
+// For the first part of preppers, where the only job is weakening the server
+export interface IWBatch {
+  weaken1: Job;
+}
+
+// For the second part of preppers, where you are maxing money and keeping security as low as possible
+export interface IGWBatch extends IGBatch {
+  weaken2: Job;
+}
+
+// For full fledged batchers
+export interface IHWGWBatch extends IGWBatch, IWBatch {
+  hack: Job;
 }
 
 export interface Job {
   readonly type: jobTypes;
-  readonly threads: number;
-  readonly server: string;
+  threads: number;
+  server: string;
 }
 
 type jobTypes = `hack` | `grow` | `weaken1` | `weaken2`;
