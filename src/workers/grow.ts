e89f1c37336be9b147e6a277859ce39f679bc3ba
DunYeeted <75920735+DunYeeted@users.noreply.github.com>
2026-02-06 16:33:51 -0600

    These scripts were changed to simply waiting until the batcher gives a start signal

--- src/workers/grow.ts
+++ src/workers/grow.ts
@@ -1,17 +1,21 @@
 import { NS } from '@ns';
-import { IWorker, TIME_BETWEEN_BATCHES, TIME_BETWEEN_JOBS } from '@/libs/controller-functions';
-import { PortErrors } from '@/libs/ExpandedNS';
+
+const [TIME_BETWEEN_BATCHES, TIME_BETWEEN_JOBS] = [1, 1];
 
 // Shouldn't be run manually
 export async function main(ns: NS) {
-  const args: IWorker = JSON.parse(ns.args[0]);
-  if (args.portNum == PortErrors.UNDEFINED_PORT_NUM_ERROR) ns.exit();
-  await ns.nextPortWrite(args.portNum);
+  const args = JSON.parse(ns.args[0] as string);
+  if (args.portNum == -4) ns.exit();
+  const port = ns.getPortHandle(args.portNum);
+  await port.nextWrite();
+  ns.tprint(port.peek());
   /** @description The endTime of the first job ran, which may or may not be the endTime */
-  const baseEndTime: number = JSON.parse(ns.peek(args.portNum)) - performance.now();
+  const baseEndTime: number = port.peek() - performance.now();
   const endTime = baseEndTime + args.batchNum * TIME_BETWEEN_BATCHES + args.jobNum * TIME_BETWEEN_JOBS;
   const waitingTime = endTime - args.workTime;
 
   await ns.grow(args.target, { additionalMsec: waitingTime });
-  ns.writePort(args.portNum, ns.pid);
+  port.write(ns.pid);
 }
+
+// ns.tprint(ns.peek(args.portNum));
