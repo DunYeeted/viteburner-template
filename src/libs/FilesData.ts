55016f5fca1a8bed18714be3f13ca2e3e8b36eb2
DunYeeted <75920735+DunYeeted@users.noreply.github.com>
2026-02-06 16:34:20 -0600

    Changed so that the paths can be used with ns.spawn, ns.exec without issue

--- src/libs/FilesData.ts
+++ src/libs/FilesData.ts
@@ -2,37 +2,37 @@ export const FilesData: {
   [file: string]: { path: string; filename: string; ramCost: number };
 } = {
   PortController: {
-    path: `./daemons/max-ports.js`,
+    path: `../daemons/max-ports.js`,
     filename: `max-ports.js`,
     ramCost: 0,
   },
   HackWorker: {
-    path: `./workers/hack.js`,
+    path: `../workers/hack.js`,
     filename: `hack.js`,
     ramCost: 1.7,
   },
   GrowWorker: {
-    path: `./workers/grow.js`,
+    path: `../workers/grow.js`,
     filename: `grow.js`,
     ramCost: 1.75,
   },
   WeakenWorker: {
-    path: `./workers/weaken.js`,
+    path: `../workers/weaken.js`,
     filename: `weaken.js`,
     ramCost: 1.75,
   },
   os: {
-    path: `./daemons/adaOS.js`,
+    path: `../daemons/adaOS.js`,
     filename: `adaOS.js`,
     ramCost: 0,
   },
   Batcher: {
-    path: `./batch-makers/shotgun-batcher.js`,
+    path: `../batch-makers/shotgun-batcher.js`,
     filename: `shotgun-batcher.js`,
     ramCost: 0,
   },
   ServerPreparer: {
-    path: `./batch-makers/server-prepper.js`,
+    path: `../batch-makers/server-prepper.js`,
     filename: `server-preppers.js`,
     ramCost: 0,
   },
