627520074dcf383d27a0c6d26ff12e53fc5ab26d
DunYeeted <75920735+DunYeeted@users.noreply.github.com>
2026-02-07 23:25:50 -0600

    Removed the debugging tprint from grow

--- src/workers/grow.ts
+++ src/workers/grow.ts
@@ -8,7 +8,6 @@ export async function main(ns: NS) {
   if (args.portNum == -4) ns.exit();
   const port = ns.getPortHandle(args.portNum);
   await port.nextWrite();
-  ns.tprint(port.peek());
   /** @description The endTime of the first job ran, which may or may not be the endTime */
   const baseEndTime: number = port.peek() - performance.now();
   const endTime = baseEndTime + args.batchNum * TIME_BETWEEN_BATCHES + args.jobNum * TIME_BETWEEN_JOBS;
