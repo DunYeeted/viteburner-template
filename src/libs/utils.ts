2d4ad2a4099642eec70fdc3bd1e8596be9e80de4
DunYeeted <75920735+DunYeeted@users.noreply.github.com>
2026-02-08 22:57:30 -0600

    Added documentation for calcGrowthFromThreads

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -8,6 +8,13 @@ export function decimalRound(num: number, placesAfterDecimal: number) {
   return Math.round(num * Math.pow(10, placesAfterDecimal)) / Math.pow(10, placesAfterDecimal);
 }
 
+/**
+ * Calculate the amount a server will grow to after an ns.grow with a certain number of threads
+ * @param currMoney Current money of the server
+ * @param threads How many growth threads a single script is using
+ * @param growthMultiplier Growth parameter of the server (from ns.getServerGrowth())
+ * @returns The server's money after a growth with the specified threads
+ */
 export function calcGrowthFromThreads(currMoney: number, threads: number, growthMultiplier: number) {
   return (currMoney + threads) * Math.exp((growthMultiplier / 100) * threads);
 }
