aa81284cf6a07b839d561a8f527dc3f90fbff152
DunYeeted <75920735+DunYeeted@users.noreply.github.com>
2026-02-10 16:12:14 -0600

    Added all special servers to Constants

---
+++ src/libs/Constants.ts
@@ -0,0 +1,20 @@
+/* Holds IP of Special Servers */
+export const SpecialServers = [
+  'home',
+  'fulcrumassets',
+  'CSEC',
+  'avmnite-02h',
+  'I.I.I.I',
+  'run4theh111z',
+  '.',
+  'The-Cave',
+  'w0r1d_d43m0n',
+  'darkweb',
+  'th3_l4byr1nth',
+  'cru3l_l4byr1nth',
+  'm3rc1l3ss_l4byr1nth',
+  'ub3r_l4byr1nth',
+  'et3rn4l_l4byr1nth',
+  'f1n4l_l4byr1nth',
+  'b0nus_l4byr1nth',
+];
