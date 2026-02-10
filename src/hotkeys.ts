07424ba65ad6dcf6e8e1ba1e6c7556ac96c00790
DunYeeted <75920735+DunYeeted@users.noreply.github.com>
2026-02-10 17:23:11 -0600

    Added buyer and connect commands

--- src/hotkeys.ts
+++ src/hotkeys.ts
@@ -1,29 +1,30 @@
+/* eslint-disable no-case-declarations */
 import { AutocompleteData, NS, ScriptArg } from '@ns';
 import { ExpandedNS } from './libs/ExpandedNS';
 import { bestTargetServer, StateBasedHotkeys } from './daemons/adaOS';
 import { PortHelpers } from './libs/Ports';
-import { SpecialServers } from './libs/Constants';
+import { PossibleRamAmts, SpecialServers } from './libs/Constants';
+import { FilesData } from './libs/FilesData';
 
 export function autocomplete(data: AutocompleteData, args: ScriptArg[]) {
-  if (args.length >= 1) {
-    return [
-      Hotkeys.root,
-      Hotkeys.scan,
-      Hotkeys.target,
-      Hotkeys.temporaryCommand,
-      Hotkeys.analyze,
-      StateBasedHotkeys.attack,
-      ...data.servers,
-    ];
-  }
-  return [
+  const baseArgs = [
     Hotkeys.root,
     Hotkeys.scan,
     Hotkeys.target,
     Hotkeys.temporaryCommand,
     Hotkeys.analyze,
+    Hotkeys.connect,
+    Hotkeys.buyer,
     StateBasedHotkeys.attack,
   ];
+
+  if (args.length >= 2) {
+    return [...PossibleRamAmts, ...data.servers];
+  }
+  if (args.length >= 1) {
+    return [...baseArgs, ...PossibleRamAmts, ...data.servers];
+  }
+  return [...baseArgs];
 }
 
 export async function main(ns: NS) {
@@ -38,7 +39,7 @@ export async function main(ns: NS) {
       let output = '';
       nsx.scanServers().forEach((server) => {
         output += `
-${server.padEnd(20)}|${ns.hasRootAccess(server) ? ` * ` : `   `}|${SpecialServers.includes(server) ? ` @ ` : `   `}`;
+${server.padEnd(20)}|${ns.hasRootAccess(server) ? ` * ` : `   `}|${SpecialServers.includes(server) && ns.getServerRequiredHackingLevel(server) <= ns.getHackingLevel() ? ` @ ` : `   `}`;
       });
 
       ns.tprint(output);
@@ -68,10 +69,46 @@ Hacking info:
   Security: ${server.hackDifficulty} / ${server.minDifficulty}
 `);
       break;
+    case Hotkeys.buyer:
+      ns.run(FilesData['ServerBuyer'].path, { threads: 1 }, ns.args[1]);
+      break;
+    case Hotkeys.connect:
+      const tree = serversTree(ns);
+      let recentServer: string = <string>ns.args[1];
+      const order = [recentServer];
+      while (recentServer !== `home`) {
+        recentServer = tree.get(recentServer) ?? ``;
+        order.unshift(recentServer);
+      }
+      let runStr = `home;`;
+      for (const server of order) {
+        runStr += `connect ${server};`;
+      }
+      ns.tprint(runStr);
+      break;
     case StateBasedHotkeys.attack:
       ns.writePort(await PortHelpers.searchForPort(nsx, `os`), StateBasedHotkeys.attack);
       break;
   }
+}
+
+function serversTree(ns: NS): Map<string, string> {
+  const servers: string[] = [`home`];
+  const serverTree: Map<string, string> = new Map();
+  serverTree.set(`home`, `done`);
+
+  for (let i = 0; i < servers.length; i++) {
+    const nextServers = ns.scan(servers[i]);
+
+    nextServers.forEach((server) => {
+      if (servers.includes(server)) return;
+
+      servers.push(server);
+      serverTree.set(server, servers[i]);
+    });
+  }
+
+  return serverTree;
 }
 
 enum Hotkeys {
@@ -80,4 +117,6 @@ enum Hotkeys {
   temporaryCommand = `temp`,
   target = `bestTarget`,
   analyze = `analyze`,
+  buyer = `buyer`,
+  connect = `shortestPath`,
 }
