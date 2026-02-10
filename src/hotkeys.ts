2ac0195e5432bda0dc68f0eec3f7dbe11e3e9031
DunYeeted <75920735+DunYeeted@users.noreply.github.com>
2026-02-10 16:12:41 -0600

    Improved autocomplete Added analyze option Improved scan

--- src/hotkeys.ts
+++ src/hotkeys.ts
@@ -2,9 +2,28 @@ import { AutocompleteData, NS, ScriptArg } from '@ns';
 import { ExpandedNS } from './libs/ExpandedNS';
 import { bestTargetServer, StateBasedHotkeys } from './daemons/adaOS';
 import { PortHelpers } from './libs/Ports';
+import { SpecialServers } from './libs/Constants';
 
-export function autocomplete(_data: AutocompleteData, _args: ScriptArg) {
-  return [Hotkeys.root, Hotkeys.scan, Hotkeys.target, Hotkeys.temporaryCommand, StateBasedHotkeys.attack];
+export function autocomplete(data: AutocompleteData, args: ScriptArg[]) {
+  if (args.length >= 1) {
+    return [
+      Hotkeys.root,
+      Hotkeys.scan,
+      Hotkeys.target,
+      Hotkeys.temporaryCommand,
+      Hotkeys.analyze,
+      StateBasedHotkeys.attack,
+      ...data.servers,
+    ];
+  }
+  return [
+    Hotkeys.root,
+    Hotkeys.scan,
+    Hotkeys.target,
+    Hotkeys.temporaryCommand,
+    Hotkeys.analyze,
+    StateBasedHotkeys.attack,
+  ];
 }
 
 export async function main(ns: NS) {
@@ -19,7 +38,7 @@ export async function main(ns: NS) {
       let output = '';
       nsx.scanServers().forEach((server) => {
         output += `
-${server.padEnd(20)}|${ns.hasRootAccess(server) ? ' * ' : '   '}`;
+${server.padEnd(20)}|${ns.hasRootAccess(server) ? ` * ` : `   `}|${SpecialServers.includes(server) ? ` @ ` : `   `}`;
       });
 
       ns.tprint(output);
@@ -33,6 +52,22 @@ ${server.padEnd(20)}|${ns.hasRootAccess(server) ? ' * ' : '   '}`;
         `Using adaOS to attack automatically does this, so only use this if you do not have enough ram for that`,
       );
       break;
+    case Hotkeys.analyze:
+      if (typeof ns.args[1] !== `string`) ns.exit();
+      // eslint-disable-next-line no-case-declarations
+      const server = ns.getServer(ns.args[1]);
+      ns.tprint(`
+
+Name: ${ns.args[1]}
+Root access: ${server.hasAdminRights ? `YES` : `NO`}
+RAM: ${ns.formatRam(server.ramUsed)} Used / ${ns.formatRam(server.maxRam)} total
+Bought: ${server.purchasedByPlayer ? `YES` : `NO`}
+Hacking info:
+  Level: ${server.hackDifficulty}
+  Money: $${ns.formatNumber(server.moneyAvailable ?? 0)} / $${ns.formatNumber(server.moneyMax ?? 0)}
+  Security: ${server.hackDifficulty} / ${server.minDifficulty}
+`);
+      break;
     case StateBasedHotkeys.attack:
       ns.writePort(await PortHelpers.searchForPort(nsx, `os`), StateBasedHotkeys.attack);
       break;
@@ -44,4 +79,5 @@ enum Hotkeys {
   scan = `scan`,
   temporaryCommand = `temp`,
   target = `bestTarget`,
+  analyze = `analyze`,
 }
