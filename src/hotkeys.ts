21dcc10b118bb11ec2c30bbdaf3476baef153d5c
DunYeeted <75920735+DunYeeted@users.noreply.github.com>
2026-02-12 16:17:14 -0600

    Moved all hotkeys logic to their own functions

--- src/hotkeys.ts
+++ src/hotkeys.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-case-declarations */
 import { AutocompleteData, NS, ScriptArg } from '@ns';
 import { ExpandedNS } from './libs/ExpandedNS';
 import { bestTargetServer, StateBasedHotkeys } from './daemons/adaOS';
@@ -35,61 +34,88 @@ export async function main(ns: NS) {
       nsx.fullRoot();
       break;
     case Hotkeys.scan:
-      // eslint-disable-next-line no-case-declarations
-      let output = '';
-      nsx.scanServers().forEach((server) => {
-        output += `
-${server.padEnd(20)}|${ns.hasRootAccess(server) ? ` * ` : `   `}|${SpecialServers.includes(server) && ns.getServerRequiredHackingLevel(server) <= ns.getHackingLevel() ? ` @ ` : `   `}`;
-      });
-
-      ns.tprint(output);
+      ns.tprint(prettyScan(nsx));
       break;
     case Hotkeys.temporaryCommand:
       ns.tprint(await nsx.tempFunction(<string>ns.args[1]));
       break;
     case Hotkeys.target:
-      const bestTarget = bestTargetServer(nsx);
-      ns.tprint(bestTarget);
-      const prepPid = ns.run(FilesData['ServerPreparer'].path, { threads: 1 }, bestTarget);
-      ns.tprint(`tail ${prepPid}`);
+      findAndAttack(nsx);
       break;
     case Hotkeys.analyze:
       if (typeof ns.args[1] !== `string`) ns.exit();
-      // eslint-disable-next-line no-case-declarations
-      const server = ns.getServer(ns.args[1]);
-      ns.tprint(`
-
-Name: ${ns.args[1]}
-Root access: ${server.hasAdminRights ? `YES` : `NO`}
-RAM: ${ns.formatRam(server.ramUsed)} Used / ${ns.formatRam(server.maxRam)} total
-Bought: ${server.purchasedByPlayer ? `YES` : `NO`}
-Hacking info:
-  Level: ${server.requiredHackingSkill}
-  Money: $${ns.formatNumber(server.moneyAvailable ?? 0)} / $${ns.formatNumber(server.moneyMax ?? 0)}
-  Security: ${ns.formatNumber(server.hackDifficulty ?? 0)} / ${ns.formatNumber(server.minDifficulty ?? 0)}
-`);
+      analyzeServer(ns, ns.args[1]);
       break;
     case Hotkeys.buyer:
       ns.run(FilesData['ServerBuyer'].path, { threads: 1 }, ns.args[1]);
       break;
     case Hotkeys.cnct:
-      const tree = serversTree(ns);
-      let recentServer: string = <string>ns.args[1];
-      const order = [recentServer];
-      while (recentServer !== `home`) {
-        recentServer = tree.get(recentServer) ?? ``;
-        order.unshift(recentServer);
-      }
-      let runStr = `home;`;
-      for (const server of order) {
-        runStr += `connect ${server};`;
-      }
-      ns.tprint(runStr);
+      if (typeof ns.args[1] !== `string`) ns.exit();
+      ns.tprint(createConnectCommand(getServerOrder(ns, ns.args[1])));
       break;
     case StateBasedHotkeys.attack:
       ns.writePort(await PortHelpers.searchForPort(nsx, `os`), StateBasedHotkeys.attack);
       break;
   }
+}
+
+function prettyScan(nsx: ExpandedNS) {
+  let output = '';
+  nsx.scanServers().forEach((server) => {
+    output += `${server.padEnd(20)}|${nsx.ns.hasRootAccess(server) ? ` * ` : `   `}`;
+    const isSpecialServer = SpecialServers.includes(server);
+    const canHack = nsx.ns.getServerRequiredHackingLevel(server) <= nsx.ns.getHackingLevel();
+    output += `${!canHack ? `   ` : isSpecialServer ? ` @ ` : ` # `}`;
+    output += `\n`;
+  });
+
+  return output;
+}
+
+function findAndAttack(nsx: ExpandedNS) {
+  const bestTarget = bestTargetServer(nsx);
+  nsx.ns.tprint(bestTarget);
+  const prepPid = nsx.ns.run(FilesData['ServerPreparer'].path, { threads: 1 }, bestTarget);
+  nsx.ns.tprint(`tail ${prepPid}`);
+}
+
+/** Returns a report on the server */
+function analyzeServer(ns: NS, server: string) {
+  const serv = ns.getServer(server);
+  const output = `
+Name: ${ns.args[1]}
+Root access: ${serv.hasAdminRights ? `YES` : `NO`}
+RAM: ${ns.formatRam(serv.ramUsed)} Used / ${ns.formatRam(serv.maxRam)} total
+Bought: ${serv.purchasedByPlayer ? `YES` : `NO`}
+Hacking info:
+  Level: ${serv.requiredHackingSkill}
+  Money: $${ns.formatNumber(serv.moneyAvailable ?? 0)} / $${ns.formatNumber(serv.moneyMax ?? 0)}
+  Security: ${ns.formatNumber(serv.hackDifficulty ?? 0)} / ${ns.formatNumber(serv.minDifficulty ?? 0)}
+`;
+  return output;
+}
+
+/** Finds the servers to connect to in order to get to a specified server */
+function getServerOrder(ns: NS, destination: string) {
+  const tree = serversTree(ns);
+  let recentServer: string = destination;
+  const order = [recentServer];
+  while (recentServer !== `home`) {
+    recentServer = tree.get(recentServer) ?? ``;
+    order.unshift(recentServer);
+  }
+
+  return order;
+}
+
+/** Returns a string for the set of connects to get to a server */
+function createConnectCommand(servers: string[]): string {
+  let runStr = `home;`;
+  for (const server of servers) {
+    runStr += `connect ${server};`;
+  }
+
+  return runStr;
 }
 
 function serversTree(ns: NS): Map<string, string> {
