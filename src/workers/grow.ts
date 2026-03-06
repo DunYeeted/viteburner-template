12c9c6cb518e19a793fdc5b9dad90f856f43d724
DunYeeted <75920735+DunYeeted@users.noreply.github.com>
2026-03-05 23:52:53 -0600

    Changed formatting Declares timing constants inside main now for the tiny optimization

--- src/workers/grow.ts
+++ src/workers/grow.ts
@@ -1,13 +1,18 @@
 import { NS } from '@ns';
 
-const [TIME_BETWEEN_BATCHES, TIME_BETWEEN_JOBS] = [1, 1];
-
 // Shouldn't be run manually
 export async function main(ns: NS) {
+  const TIME_BETWEEN_BATCHES = 1;
+  const TIME_BETWEEN_JOBS = 1;
+
   const args = JSON.parse(ns.args[0] as string);
+
+  // Batchers' portNum is -4 when undefined
   if (args.portNum == -4) ns.exit();
   const port = ns.getPortHandle(args.portNum);
-  await port.nextWrite();
+
+  if (port.empty()) await port.nextWrite();
+
   /** @description The endTime of the first job ran, which may or may not be the endTime */
   const baseEndTime: number = port.peek() - performance.now();
   const endTime = baseEndTime + args.batchNum * TIME_BETWEEN_BATCHES + args.jobNum * TIME_BETWEEN_JOBS;
